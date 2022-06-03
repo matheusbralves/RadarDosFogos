6a46900a7144e442cab7ce126c5437bd6c0e90ee
j-novaes <joaovictor.nb21@hotmail.com>
2022-06-03 16:12:51 +0100

    Editei DashBoard

--- app/src/main/java/pt/ulusofona/deisi/cm2122/g21800876_21900074/DashboardFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21800876_21900074/DashboardFragment.kt
@@ -19,10 +19,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import pt.ulusofona.deisi.cm2122.g21800876_21900074.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), OnLocationChangedListener  {
     private lateinit var binding: FragmentDashboardBinding
     private lateinit var viewModel : FireViewModel
     private val adapter = FireListAdapter(onClick = ::onItemClick)
+    private var userLat = 0.0;
+    private var userLng = 0.0;
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         //Adicionar isso em todos os fragmentos pra ficar com o titulo certo na barra laranja
@@ -31,6 +33,7 @@ class DashboardFragment : Fragment() {
         val view = inflater.inflate(
             R.layout.fragment_dashboard, container, false
         )
+        FusedLocation.registerListener(this)
         binding = FragmentDashboardBinding.bind(view)
         viewModel = ViewModelProvider(this).get(FireViewModel::class.java)
         return binding.root
@@ -45,6 +48,11 @@ class DashboardFragment : Fragment() {
         viewModel.getAllFires { updateList(it) }
     }
 
+    override fun onLocationChanged(latitude: Double, longitude: Double) {
+        userLat = latitude
+        userLng = longitude
+    }
+
     private fun onItemClick(fire: FireParcelable) {
         NavigationManager.goToDetaisFragment(parentFragmentManager, fire)
     }
@@ -57,6 +65,16 @@ class DashboardFragment : Fragment() {
 
     private fun radiusSpinnerSetup(){
         val spinner: Spinner = binding.spinner
+
+        spinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View, i: Int, l: Long) {
+                updateList(getFiresByMaxDistance(userLat,userLng,spinner.selectedItem.toString()))
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+                return
+            }
+        })
         ArrayAdapter.createFromResource(
             requireActivity(),
             R.array.radius_array,
@@ -67,22 +85,43 @@ class DashboardFragment : Fragment() {
         }
     }
 
-    private fun radiusSelect(){
-        when (binding.spinner.selectedItem.toString()) {
-            "25 KM" -> {
-                //viewModel.getDashboardRegistros25 { updateList(it) }
-            }
+    fun getFiresByMaxDistance(userLat:Double, userLng:Double, maxDistance: String): MutableList<FireParcelable> {
+        val allFires = viewModel.getAllFiresList()
+        val chosenFires = mutableListOf<FireParcelable>()
+        var distance = 0.0
 
-            "35 KM" -> {
-                //viewModel.getDashboardRegistros35 { updateList(it) }
-            }
-
-            "50 KM" -> {
-                //viewModel.getDashboardRegistros50 { updateList(it) }
-            }
-
-            else -> print("nada")
+        when(maxDistance) {
+            "300 KM" -> distance = 300.00
+            "150 KM" -> distance = 150.00
+            "50 KM" -> distance = 50.00
         }
+
+        for(fire in allFires) {
+            if(defineDistance(userLat, userLng, fire.lat,fire.lng) <= distance) {
+                chosenFires.add(fire)
+            }
+        }
+
+        return chosenFires
+    }
+
+    fun defineDistance(userLat:Double, userLng:Double, fireLat:Double, fireLng:Double):Double {
+        var earthRadiusKm = 6371;
+
+        var dLat = degreesToRadians(userLat-fireLat);
+        var dLon = degreesToRadians(userLng-fireLng);
+
+        var convertedUserLat = degreesToRadians(userLat);
+        var convertedFireLat = degreesToRadians(fireLat);
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(convertedUserLat) * Math.cos(convertedFireLat);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return earthRadiusKm * c;
+    }
+
+    fun degreesToRadians(degrees:Double):Double {
+        return degrees * Math.PI / 180;
     }
 
     private fun callHelp(){
