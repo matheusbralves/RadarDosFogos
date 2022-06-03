7557e58e2aa506cb96917d4512c829e893925fb7
matheusbralves <matheusbralvess@gmail.com>
2022-06-03 15:04:40 +0100

    Dropdown para filtrar por Distrito

--- app/src/main/java/pt/ulusofona/deisi/cm2122/g21800876_21900074/FireListFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21800876_21900074/FireListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,6 +28,7 @@ class FireListFragment : Fragment() {
             NavigationManager.goToFireMapFragment(
                 it1.supportFragmentManager)
         } }
+        districtSpinnerSetup()
         binding.fogosActivosCount.text = viewModel.getActiveFire().size.toString()
         binding.fogosActivosCount.text = viewModel.getDistrictWithMostFires()
         viewModel.getAllFires { updateList(it) }
@@ -51,5 +54,17 @@ class FireListFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(FireViewModel::class.java)
         binding = FragmentFireListBinding.bind(view)
         return binding.root
+    }
+
+    private fun districtSpinnerSetup(){
+        val spinner: Spinner = binding.spinner
+        ArrayAdapter.createFromResource(
+            requireActivity(),
+            R.array.district_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
